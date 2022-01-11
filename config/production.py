c6ec58409f3e0303e54c24cd001a8233b002388f
정용선 <cjfrnjs123@naver.com>
2022-01-11 13:40:46 +0900

    log set

--- config/production.py
+++ config/production.py
@@ -1,5 +1,29 @@
 from config.default import *
+from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\x9b\xfa\xc2\xb3\xf6\x024*_h\x00"\x8c2\x0f\xd6'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -3,10 +3,7 @@ from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-@bp.route('/hello')
-def hello_pybo():
-    return 'Hello, Pybo!'
-
 @bp.route('/')
 def index():
+    3/0  # 강제로 오류발생
     return redirect(url_for('question._list'))
